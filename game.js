1f2782b94819994b8dabfcc319a8bbe3ee3b9e12
Bret Balletta <bballetta@tesla.com>
2024-09-05 12:31:47 -0500

    Add playRound function and change call logic

--- game.js
+++ game.js
@@ -3,26 +3,44 @@ let humanScore = 0;
 
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3 + 1);
-    console.log(randomInt)
+    console.log(randomInt);
+    let computerChoice = "";
     if (randomInt === 1) {
       computerChoice = "Rock";
-    }else if (randomInt ===2) {
+    }else if (randomInt === 2) {
       computerChoice = "Paper";
     }else {
       computerChoice = "Scissors";
     }
     console.log(computerChoice);
+    return computerChoice;
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Select a weapon! Rock, paper or scissors!",
-           "Type rock, paper or scissors to choose.");
+           "Type rock, paper or scissors to choose.").toLowerCase();
     console.log(humanChoice);
+    return humanChoice;
 }
 
-function playRound() {
-
+function playRound(computerChoice,humanChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    if (computerChoice === "Rock") {
+      if (humanChoice === "scissors") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore ++;
+      }  
+      else if (humanChoice === "paper") {
+        console.log("You win! Paper beats Rock!");
+        humanScore ++;
+      }
+      else {
+        console.log("Tie! You both chose Rock!")
+      }
+    }
 }
 
-getComputerChoice()
-getHumanChoice()
+/*getComputerChoice();
+getHumanChoice();*/
+playRound()
