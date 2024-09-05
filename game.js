b323952f0e9a830e44e542fbeff11d9b0b13d2a0
Bret Balletta <bballetta@tesla.com>
2024-09-05 10:40:11 -0500

    Add getComputer and getHumanChoice functions

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3 + 1);
+    console.log(randomInt)
+    if (randomInt === 1) {
+      weapon = "Rock";
+    }else if (randomInt ===2) {
+      weapon = "Paper";
+    }else {
+      weapon = "Scissors";
+    }
+    console.log(weapon);
+}
+
+function getHumanChoice() {
+    let userWeapon = prompt("Select a weapon! Rock, paper or scissors!",
+           "Press r, p or s to choose.");
+    console.log(userWeapon);
+}
+
+getComputerChoice()
+getHumanChoice()
